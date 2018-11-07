a899363bcfac9795b114d7ee0ae2372ccb47c998
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-11-07 15:21:43 -0600

    add cgi-bin/ClientConfigAWE.pm

---
+++ cgi-bin/ClientConfigAWE.pm
@@ -0,0 +1,20 @@
+package ClientConfigAWE;
+
+require Exporter;
+
+# This is the base url of the authServer
+use constant BASE_URL => $ENV{'BASE_URL'};
+
+# These values must correspond to what the app was registered with on the
+# application registration page of the authServer
+use constant APPLICATION_NAME   => $ENV{'APPLICATION_NAME'};
+use constant APPLICATION_URL    => $ENV{'APPLICATION_URL'};
+use constant APPLICATION_SECRET => $ENV{'APPLICATION_SECRET'};
+
+# This is the name of the cookie the user information will be stored in
+use constant COOKIE_NAME => 'AuthWebSession';
+
+@ISA = qw(Exporter);
+@EXPORT = qw(BASE_URL APPLICATION_NAME APPLICATION_URL APPLICATION_SECRET COOKIE_NAME);
+
+1;
